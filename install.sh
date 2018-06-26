0d2abdd9f8e44d8dafedf913786971919b230734
Shadowbeetle <tamas.kadlecsik@gmail.com>
2018-06-26 09:12:47 +0200

    fix argument check

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 set -eo pipefail
 
+if [ -z $1 ]; then
+  echo "path expected, run ./isntall.sh to see usage"
+  exit 0
+fi
+
 if [ $1 = "-h" ]; then
     echo "Usage: ./install.sh <install-path>"
     exit 0
@@ -16,3 +21,5 @@ cd $1
 
 rm $1/fourcut 2>/dev/null || true
 ln -s $source_path/four_cut.py fourcut
+
+echo "executable created in $1"
